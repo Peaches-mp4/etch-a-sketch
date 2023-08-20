b724c9c7c72a0cdbebf0ee303907dbaf3bba4fd6
Peaches-mp4 <131920771+Peaches-mp4@users.noreply.github.com>
2023-08-21 00:46:47 +0200

    Create basic skeleton and connect files

---
+++ javascript.js
@@ -0,0 +1,6 @@
+//Create a webpage with a 16x16 grid of square divs.
+// Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
+// It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
+
+//mogu da probam sa document create element koji cu da loopujem odredjeni broj puta; broj puta stavim u var da bi to moglo kasnije da se prilagodjava; sve divove appendujem container divu
+
