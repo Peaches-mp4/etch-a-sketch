11cdb6ed60da47dd87c46c9971a2b35bf3cb212e
Peaches-mp4 <131920771+Peaches-mp4@users.noreply.github.com>
2023-08-22 01:40:03 +0200

    Add function to color squares on hover

--- javascript.js
+++ javascript.js
@@ -1,19 +1,21 @@
-//Create a webpage with a 16x16 grid of square divs.
-// Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
-// It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
-
-//mogu da probam sa document create element koji cu da loopujem odredjeni broj puta; broj puta stavim u var da bi to moglo kasnije da se prilagodjava; sve divove appendujem container divu
 
 //initial declarations
 
 let container = document.getElementById('container');
-container.style.cssText = "width: 200pp; height: 200px; background-color: yellow;";
-let numOfDivs = 16;
+let numOfDivs = 16*16; 
 
-//for loop to create squares
+//for loop to create squares, add event listeners
 
 for (let i = 0; i < numOfDivs; i++) {
     let square = document.createElement('div');
-    square.style.cssText = "width: 30px; height: 30px; border: 3px solid black; background-color: pink;";
+    square.classList.add('divSquare');
     container.appendChild(square);
+    square.addEventListener('mouseover', colorFunction);
 }
+
+//function to color squares
+
+    function colorFunction(e) {
+    e.target.classList.add("colored-black");
+}
+
