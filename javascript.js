e07272342437f5ad672a503227964aac4781146e
Peaches-mp4 <131920771+Peaches-mp4@users.noreply.github.com>
2023-08-27 23:07:52 +0200

    Solve issues with grid sizing

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
 
 //initial declarations
-//let body = document.getElementsByTagName('body');
 let container = document.getElementById('container');
 let clearButton = document.getElementById('clear');
 let numOfDivs = 16; 
 createGrid();
 
+
 //popup asking for the size of the grid
 clearButton.addEventListener('click', popupFunction);
 
 function popupFunction() {
-   numOfDivs = prompt('How many squares per side do you want your grid to have?', 16);
+   numOfDivs = prompt('How many squares per side do you want your sketchpad to have?', 16);
+
    if(numOfDivs > 100) {
-    numOfDivs = prompt('Please choose a lower number');
-   }
+    numOfDivs = prompt('Please choose a lower number');}
+
    console.log(numOfDivs)
    container.innerHTML = '';
    createGrid();
@@ -25,14 +26,11 @@ function createGrid() {
     for (let i = 0; i < numOfDivs*numOfDivs; i++) {
     let square = document.createElement('div');
     square.classList.add('divSquare');
+    container.style.gridTemplateColumns = `repeat(${numOfDivs}, 1fr)`;
     container.appendChild(square);
     square.addEventListener('mouseover', colorFunction);
-        console.log('grid created')
-
     }
-
 }
-
 
 //function to color squares
 
